dd9e62c818fa97fcc7460f1589a597e60f52c09e
Maycon Carlete <maycon.carlete@gmail.com>
2022-01-22 11:31:51 -0300

    feat: using typescript to list dynamoDB tables

---
+++ index.ts
@@ -0,0 +1,66 @@
+import aws from 'aws-sdk'
+import DynamoDB, { CreateTableInput, CreateTableOutput } from 'aws-sdk/clients/dynamodb'
+
+
+
+class DynamoModel {
+  dynamoDb: DynamoDB
+  constructor(){
+     this.dynamoDb = new aws.DynamoDB(
+       {
+          apiVersion: '2012-08-10',
+          region: 'us-east-1',
+          endpoint: 'http://localhost:8000'
+        }
+      )
+  }
+
+  async createTable(tableData: CreateTableInput){
+    try{
+      const result = await this.dynamoDb.createTable(tableData).promise()
+      console.log(result)
+      return result
+    } catch(e) {
+      console.log('Deu erro aqui')
+      console.log(e)
+    }
+  }
+
+  async listTables(){
+    const tables = await this.dynamoDb.listTables().promise()
+    return tables
+  }
+}
+
+const tableData:CreateTableInput = {
+  TableName:'Meu test',
+  AttributeDefinitions:[
+    {
+      AttributeName: 'Artiist',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'SongTitle',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema:[
+    {
+      AttributeName: 'Artist',
+      KeyType: 'HASH'
+    },
+    {
+      AttributeName: 'SongTitle',
+      KeyType: 'RANGE'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  }
+}
+
+const dynamoRepository = new DynamoModel()
+dynamoRepository.listTables().then(console.log)
+
+console.log('Hello world')
